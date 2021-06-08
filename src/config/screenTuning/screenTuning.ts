c522a64a7a4aa1c8b8205a58c7eb0ecdf706b215
Will Hannagan <44087247+hgnme@users.noreply.github.com>
2021-06-08 22:37:19 +1000

    Screen white-balance

--- src/config/screenTuning/screenTuning.ts
+++ src/config/screenTuning/screenTuning.ts
@@ -1,16 +1,16 @@
 import { displaySize } from '../../properties';
 
 const kombiTuning = [
-  { name: 'color_red_factor', value: '256', type: 'fixed8' },
-  { name: 'color_green_factor', value: '256', type: 'fixed8' },
+  { name: 'color_red_factor', value: '240', type: 'fixed8' },
+  { name: 'color_green_factor', value: '240', type: 'fixed8' },
   { name: 'color_blue_factor', value: '256', type: 'fixed8' },
   { name: 'color_saturation_factor', value: '256', type: 'fixed8' },
 ];
 
 const mib2Tuning = [
-  { name: 'color_red_factor', value: '256', type: 'fixed8' },
+  { name: 'color_red_factor', value: '242', type: 'fixed8' },
   { name: 'color_green_factor', value: '256', type: 'fixed8' },
-  { name: 'color_blue_factor', value: '256', type: 'fixed8' },
+  { name: 'color_blue_factor', value: '242', type: 'fixed8' },
   { name: 'color_saturation_factor', value: '256', type: 'fixed8' },
 ]
 
